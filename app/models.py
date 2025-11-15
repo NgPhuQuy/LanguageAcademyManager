9b336edaf9a5f8231ca7b127b07b252684d8c1b8
quy <ngphuquy241@gmail.com>
2025-11-15 20:46:50 +0700

    them models(gan hoan thien)

--- app/models.py
+++ app/models.py
@@ -1,13 +1,8 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey
+from sqlalchemy.orm import relationship
 from flask_login import UserMixin
 from enum import Enum as RoleEnum
-
 from app import db
-
-class Base(db.Model):
-    __abstract__ = True
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name=Column(String(255), nullable=False)
 
 
 class UserRole(RoleEnum):
@@ -17,24 +12,56 @@ class UserRole(RoleEnum):
     CASHIER = 4
 
 
-# class Student(Base):
-#     abc =Column()
-#
-# class Teacher(Base):
-#     abc = Column()
-#
-# class Cashier(Base):
-#     abc = Column()
+class Base(db.Model):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
 
-class User(Base, UserMixin):
-    username = Column()
-    password = Column()
+
+class User(Base):
+    account = relationship('Account', backref="User", lazy=True)
+    email = Column(String(255), nullable=False, )
+
+
+class Account(UserMixin):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
     role = Column(Enum(UserRole), default=UserRole.STUDENT)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+
+
+class Student(User):
+    pass
+
+
+class Teacher(User):
+    myClass = relationship('MyClass', backref="Teacher", lazy=True)
+
+
+class Cashier(User):
+    bill = relationship('Bill', backref="Cashier", lazy=True)
 
 
 class Language(Base):
+    course = relationship('Course', backref="Language", lazy=True)
 
 
-class Bill():
+class Bill(Base):
+    cashier_id = Column(Integer, ForeignKey(Cashier.id), nullable=False)
+
 
 class Course(Base):
+    lang_id = Column(Integer, ForeignKey(Language.id), nullable=False)
+
+
+class MyClass(Base):
+    teacher_id = Column(Integer, ForeignKey(Teacher.id), nullable=False)
+
+
+class Fee(Base):
+    fee = Column(Integer)
+
+
+class Score(Base):
+    score = Column(Integer)
